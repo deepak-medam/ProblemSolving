50db07cccfbb31de32433c66dd11890106a36257
Restyled.io <commits@restyled.io>
2024-05-30 12:41:44 +0000

    Restyled by reorder-python-imports

--- Strings/02_reverse_string.py
+++ Strings/02_reverse_string.py
@@ -1,14 +1,9 @@
 # s1 = input("Enter a string to reverse: ")
-
-
 # ################################ Method 01 #################################
 # rev = ""
 # for char in s1:
 #     rev = char + rev
-
 # print(f"This is solution from method 01: {rev}")
-
 # ################################ Method 02 #################################
-
 # s1 = s1[::-1]
 # print(f"This is a solution from method 02: {s1}")
