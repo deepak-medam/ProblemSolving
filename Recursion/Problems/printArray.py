954d38669a503386e369f4cfe12327b7114fa3f1
Restyled.io <commits@restyled.io>
2024-06-01 07:21:27 +0000

    Restyled by reorder-python-imports

--- Recursion/Problems/printArray.py
+++ Recursion/Problems/printArray.py
@@ -1,28 +1,21 @@
 # Print Array Elements Using Recursion
 # You are given an array arr of size n. You need to print the array elements from start to end
 # using given recursive function.
-
-
 # Example 1:
 # Input:
 # n = 5
 # arr[] = {1,2,3,4,5}
 # Output: 1 2 3 4 5
-
 # Example 2:
 # Input:
 # n = 4
 # arr[] = {5,4,2,1}
 # Output: 5 4 2 1
-
 # Your Task:
 # Complete the function printArrayRecursively() that takes array and size n as parameters
 # and prints the array elements recursively. The newline is provided by driver code.
-
 # Expected Time Complexity: O(N).
 # Expected Auxiliary Space: O(N) (Recursive).
-
-
 # Constraints:
 # 1 <= n <= 100
 def printArrayRecursively(arr, n):
--- Recursion/Problems/recursriveSum.py
+++ Recursion/Problems/recursriveSum.py
@@ -1,23 +1,17 @@
 # Recursively Sum N Numbers
 # You are given a number n. You need to recursively sum the numbers from 1 to n and return the sum.
-
 # Example 1:
-
 # Input:
 # n = 5
 # Output: 15
 # Example 2:
-
 # Input:
 # n = 4
 # Output: 10
 # Your Task:
-
 # Complete the function recursiveSum that takes n as paramenter and return the sum.
-
 # Expected Time Complexity: O(N).
 # Expected Auxiliary Space: O(N) (Recursive).
-
 # Constraints:
 # 0 <= n <= 100
 
